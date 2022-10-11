f6093dc78e09ee20d83a8c19a352217b966598ab
nagasaisameeta <nagasais@sidgs.com>
2022-10-11 16:13:54 +0530

    sai modified

--- Application.java
+++ Application.java
@@ -21,14 +21,6 @@
 			else
 			{
 				System.out.println("file already exists");
-	}
-
-	public static void printOne() {
-		System.out.println("Hello World");
-	}
-
-	public static void printTwo() {
-		printOne();
-		printOne();
+	
 	}
 }
